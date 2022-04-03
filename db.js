696e8c51ec1f453cc2861ea04919e3a414cb49dc
NurislamAituarov <nurislamajtuarov@gmail.com>
2022-04-03 15:49:11 +0600

    initializing

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'auto_shop',
-});
+
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      user: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'auto_shop',
+    };
+
+const pool = new Pool(poolConfig);
 
 export default pool;
